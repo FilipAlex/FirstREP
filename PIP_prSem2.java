72bc30129cc3bf813ec7208499d3e3372ba2b444
FilipAlex <alexandru-gabriel.filip@student.tuiasi.ro>
2022-03-15 16:44:54 +0200

    Primul repository pentru PIP proiect

---
+++ PIP_prSem2.java
@@ -0,0 +1,12 @@
+package PIP_PRoiect_sem2;
+
+public class PIP_prSem2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Folder PIP-proiect! \n");
+
+	}
+
+}
